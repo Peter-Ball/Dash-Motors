873dbb84d227b6a90be5c1ed8820c7d86f2832d3
Peter Ball <peterball12@gmail.com>
2019-03-06 01:32:18 -0500

    Implemented basic callback functionality

--- app.py
+++ app.py
@@ -4,11 +4,17 @@ import numpy as np
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
 
+#setup
 app = dash.Dash(__name__)
 
 df = pd.read_csv("mtcars.tsv", sep='\t', skiprows=4)
 
+print(df)
+
+#Define the layout
 app.layout = html.Div([
 	html.H1(children='Dash Motors!'),
 
@@ -18,6 +24,7 @@ app.layout = html.Div([
 
 	html.Div([
 		dcc.Dropdown(
+			id='drop-down',
 			options=[
 			{'label': 'Miles/Gallon', 'value': 'mpg'},
 			{'label': 'Horsepower', 'value': 'hp'},
@@ -28,6 +35,32 @@ app.layout = html.Div([
 
 	dcc.Graph(id='Car Graph')
 ])
+
+
+#Make the graph
+@app.callback(
+	Output(component_id='Car Graph', component_property='figure'), 
+	[Input(component_id='drop-down', component_property='value')]
+)
+def update_figure(value):
+	#isolate for the selected parameter
+	df_segment = df[['model', value]]
+	
+	return{
+		'data': [go.Bar(
+			x=df_segment['model'].tolist(),
+			y=df_segment[value].tolist()
+			)],
+		'layout': go.Layout(
+			xaxis={
+				'title': 'Model'
+			},
+			yaxis={
+				'title': value
+			}
+		)
+	}
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
